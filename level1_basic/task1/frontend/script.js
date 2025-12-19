d59df47337499bd200781f9c7321cdac0a21bf0f
ibraum <ibraumodnok@gmail.com>
2025-12-19 19:40:21 +0000

    task 3 of level 1

--- level1_basic/task1/frontend/script.js
+++ level1_basic/task1/frontend/script.js
@@ -3,6 +3,8 @@ const hostname = "localhost";
 const protocol = "http";
 
 let usersDiv = document.getElementById('users');
+const blurModals = document.getElementById('blur-modals');
+const form = document.getElementById('form');
 let users = [];
 
 function construct_url (id) {
@@ -14,11 +16,38 @@ function construct_url (id) {
 }
 
 async function getUsers () {
-    const res = await fetch(construct_url());
-    const result = await res.json();
-    console.log(result);
-    users = result
-    //usersDiv.innerText = JSON.stringify(users)
+  const res = await fetch(construct_url());
+  const result = await res.json();
+  console.log(result);
+
+  const users = result;
+  let table = "";
+
+  users.data.forEach(user => {
+    table += `
+      <tr>
+        <td>${user.id}</td>
+        <td>${user.lastname}</td>
+        <td>${user.firstname}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td>${user.age}</td>
+        <td>${user.field}</td>
+      </tr>
+    `;
+  });
+
+  usersDiv.innerHTML = table;
 }
 
+
 getUsers()
+
+function toggleModal () {
+
+}
+
+blurModals.addEventListener('click', function () {
+    blurModals.classList.toggle('show');
+    form.classList.toggle('show');
+});
