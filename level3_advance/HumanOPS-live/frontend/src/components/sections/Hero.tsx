72aa470aee39361cebc7018c51f8ad1ea5a9ed0d
ibraum <ibraumodnok@gmail.com>
2026-01-04 16:43:47 +0000

    Feat: LoginPage, SignupPage, NotFoundPage, Home loading OK

--- level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
@@ -3,12 +3,12 @@ const Hero = () => {
     <>
       <div className="w-full h-screen bg-[url('/src/assets/hero-bg.png')] bg-cover bg-center bg-no-repeat">
         <div className="max-w-[900px] w-full h-full mx-auto flex flex-col items-center justify-end pb-30 gap-5">
-          <div className="dm-sans-medium text-8xl font-bold mt-50 text-center text-transparent bg-clip-text bg-[linear-gradient(-150deg,white,white,#7a7a7a)]">
+          <div className="dm-sans-medium md:text-8xl text-4xl font-bold mt-50 text-center text-transparent bg-clip-text bg-[linear-gradient(-150deg,white,white,#7a7a7a)]">
             Operational people management for modern teams
           </div>
-          <div className="text-xl dm-sans-medium text-white">HumanOps Live connects people, skills, and operations in one intelligent system.</div>
+          <div className="md:text-xl text-lg dm-sans-medium text-white">HumanOps Live connects people, skills, and operations in one intelligent system.</div>
           <div className="mt-4">
-            <button className="dm-sans-medium text-xl text-black bg-white px-10 py-4 rounded-full hover:shadow-md hover:shadow-white/40 duration-300 cursor-pointer">Get Started</button>
+            <button className="dm-sans-medium md:text-xl text-lg text-black bg-white px-10 py-4 rounded-full hover:shadow-md hover:shadow-white/40 duration-300 cursor-pointer">Get Started</button>
           </div>
         </div>
       </div>
