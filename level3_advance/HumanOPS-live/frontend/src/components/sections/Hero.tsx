2109a9b9062a54c0f3694cb05af67c0835d2fb0c
ibraum <ibraumodnok@gmail.com>
2026-01-04 16:51:07 +0000

    Feat: LoginPage, SignupPage, NotFoundPage, Home loading OK

--- level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <>
       <div className="w-full h-screen bg-[url('/src/assets/hero-bg.png')] bg-cover bg-center bg-no-repeat">
         <div className="max-w-[900px] w-full h-full mx-auto flex flex-col items-center justify-end pb-30 gap-5">
-          <div className="dm-sans-medium md:text-8xl text-5xl font-bold mt-50 text-center text-transparent bg-clip-text bg-[linear-gradient(-150deg,white,white,#7a7a7a)]">
+          <div className="dm-sans-medium md:text-8xl text-4xl font-bold mt-50 text-center text-transparent bg-clip-text bg-[linear-gradient(-150deg,white,white,#7a7a7a)]">
             Operational people management for modern teams
           </div>
           <div className="md:text-xl text-lg dm-sans-medium text-white text-center">HumanOps Live connects people, skills, and operations in one intelligent system.</div>
