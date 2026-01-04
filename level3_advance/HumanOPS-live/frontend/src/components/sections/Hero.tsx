0e5a0b21114b12c793d98d7495a289faf6a39bb0
ibraum <ibraumodnok@gmail.com>
2026-01-04 13:14:49 +0000

    Adding navbar and hero

--- level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className="w-full h-screen bg-[url('/src/assets/hero-bg.png')] bg-cover bg-center bg-no-repeat">
+        <div className="max-w-[700px] w-full h-full mx-auto flex items-center justify-center">
+          <div className="font-inter text-8xl font-bold text-white mt-10">HumanOps-Live</div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- level3_advance/HumanOPS-live/frontend/src/components/sections/Navbar.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/sections/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="w-full h-[40px] mt-14 fixed top-0 left-0 right-0">
+        <div className="max-w-[620px] w-full h-full mx-auto grid grid-cols-[150px_1fr_100px] place-items-center bg-white/10 backdrop-blur-xs rounded-full py-1 px-1">
+          <div className="text-md text-white font-semibold">HumanOps-Live</div>
+          <div className="grid grid-cols-3 w-full place-items-center text-sm text-white/80">
+                      <div className=""><Link to="/" className="w-full py-1 px-2 hover:bg-white/10 rounded-full duration-300">Home</Link></div>
+          <div className=""><Link to="/about" className="w-full py-1 px-2 hover:bg-white/10 rounded-full duration-300">About</Link></div>
+          <div className=""><Link to="/contact" className="w-full py-1 px-2 hover:bg-white/10 rounded-full duration-300">Contact</Link></div>
+          </div>
+          <div className="text-white font-semibold"><Link to="/login" className="w-full px-2 pl-6 border-l border-white/50">Login</Link></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
