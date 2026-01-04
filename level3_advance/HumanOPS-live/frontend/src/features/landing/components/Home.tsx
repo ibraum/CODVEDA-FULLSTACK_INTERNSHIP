e83877619fd4befef3f2925fa9a29f57323bcc2c
ibraum <ibraumodnok@gmail.com>
2026-01-04 11:26:03 +0000

    Adding navbar and hero

--- level3_advance/HumanOPS-live/frontend/src/features/landing/components/Home.tsx
+++ level3_advance/HumanOPS-live/frontend/src/features/landing/components/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from '../../../components/sections/Hero'
+import Navbar from '../../../components/sections/Navbar'
 
 const Home = () => {
   return (
     <>
+        <Navbar/>
         <Hero/>
     </>
   )
