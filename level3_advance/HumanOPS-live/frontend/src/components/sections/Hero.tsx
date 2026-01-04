6bf4c34948d36739ed4d9a00ad1b709dfac40d2b
ibraum <ibraumodnok@gmail.com>
2026-01-04 16:44:15 +0000

    Feat: LoginPage, SignupPage, NotFoundPage, Home loading OK

--- level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
           <div className="dm-sans-medium md:text-8xl text-4xl font-bold mt-50 text-center text-transparent bg-clip-text bg-[linear-gradient(-150deg,white,white,#7a7a7a)]">
             Operational people management for modern teams
           </div>
-          <div className="md:text-xl text-lg dm-sans-medium text-white">HumanOps Live connects people, skills, and operations in one intelligent system.</div>
+          <div className="md:text-xl text-lg dm-sans-medium text-white text-center">HumanOps Live connects people, skills, and operations in one intelligent system.</div>
           <div className="mt-4">
             <button className="dm-sans-medium md:text-xl text-lg text-black bg-white px-10 py-4 rounded-full hover:shadow-md hover:shadow-white/40 duration-300 cursor-pointer">Get Started</button>
           </div>
