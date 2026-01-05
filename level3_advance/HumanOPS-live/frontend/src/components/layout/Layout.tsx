a80b04b7816292b92a6afff17d5408698baddf89
ibraum <ibraumodnok@gmail.com>
2026-01-05 00:07:13 +0000

    Feat: LoginPage, SignupPage, NotFoundPage, Home loading OK

--- level3_advance/HumanOPS-live/frontend/src/components/layout/Layout.tsx
+++ level3_advance/HumanOPS-live/frontend/src/components/layout/Layout.tsx
@@ -11,36 +11,212 @@ const Layout = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-neutral-900 text-white">
-      <header className="border-b border-neutral-800 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <Link to="/" className="text-xl font-bold text-blue-400">
-            HumanOps Live
-          </Link>
-          <nav className="space-x-4 flex items-center">
-            <Link
-              to="/dashboard"
-              className="hover:text-blue-300 transition-colors"
-            >
-              Dashboard
-            </Link>
-            <button
+    <div className="min-h-screen grid grid-rows-[280px_2fr] bg-white">
+      <header className="bg-neutral-100 py-6 px-12 flex justify-between items-center ">
+        <div className="w-full max-w-[750px] h-full flex flex-col justify-between items-start">
+          <div className="flex items-center gap-4">
+            <div className="h-14 w-14 rounded-full bg-white flex items-center justify-center shadow cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+                />
+              </svg>
+            </div>
+            <div
               onClick={handleLogout}
-              className="hover:text-blue-300 transition-colors cursor-pointer"
-            >
-              Logout
+              className="h-14 w-14 rounded-full bg-black"
+            ></div>
+            <div className="h-14 pt-1">
+              <div className="text-lg">HumanOps-Live</div>
+              <div className="text-sm text-neutral-500">Dashboard</div>
+            </div>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="dm-sans-medium h-22 w-22 rounded-full flex items-center justify-center text-3xl font-semibold text-neutral-900 border border-neutral-300">
+              04
+            </div>
+            <div className="text-md text-neutral-900">
+              <div className="">Sun,</div>
+              <div className="">January</div>
+            </div>
+            <div className="h-10 w-[1px] bg-neutral-300"></div>
+            <button className="cursor-pointer text-white text-sm h-12 px-8 bg-black hover:bg-neutral-700 duration-300 rounded-full flex items-center gap-2">
+              Show my Teams{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                />
+              </svg>
             </button>
-          </nav>
+            <button className="cursor-pointer text-white h-12 w-12 border border-neutral-300 rounded-full flex items-center justify-center relative text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 text-black"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
+                />
+              </svg>
+              <div className="absolute w-2 h-2 bg-red-500 rounded-full top-1 right-0"></div>
+            </button>
+          </div>
+        </div>
+        <div className="w-full max-w-[750px] h-full flex flex-col justify-between items-start">
+          <div className="w-full flex items-center justify-between gap-6">
+            <div className="flex items-center gap-6">
+              <div className="h-14 w-14 rounded-full border border-neutral-300 flex items-center justify-center cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+                  />
+                </svg>
+              </div>
+              <div className="h-14 w-14 rounded-full bg-black bg-[url('/src/assets/profil-bg.png')] bg-cover bg-center border border-neutral-300 shadow"></div>
+              <div className="h-14 pt-1 dm-sans-regular">
+                <div className="text-lg">Kondo Ibrahim</div>
+                <div className="text-sm">Admin RH</div>
+              </div>
+            </div>
+            <div className="flex items-center gap-6">
+              <div className="h-14 w-14 flex items-center justify-center rounded-full border border-neutral-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </div>
+              <div className="dm-sans-regular text-neutral-500">
+                Start searching here...
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-between w-full gap-6">
+            <div className="dm-sans-light text-neutral-400">
+              <div className="text-5xl">Hey, Welcome back </div>
+              <div className="text-5xl text-neutral-900">Kondo Ibrahim !</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="h-20 w-20 rounded-full border border-neutral-300 flex items-center justify-center cursor-pointer hover:bg-black duration-300 group relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 group-hover:text-white duration-300"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                  />
+                </svg>
+
+                <div className="h-20 group-hover:h-64 orverflow-hidden w-20 rounded-full grid group-hover:grid-rows-3 gap-1 cursor-pointer duration-300 group absolute top-0 group-hover:top-22 duration-300">
+                  <div className="h-20 w-20 rounded-full border border-neutral-300 flex items-center justify-center cursor-pointer hover:bg-neutral-300 duration-300 group-hover:flex hidden">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="h-20 w-20 rounded-full border border-neutral-300 flex items-center justify-center cursor-pointer hover:bg-neutral-300 duration-300 group-hover:flex hidden">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  </div>
+                  <div onClick={handleLogout} className="h-20 w-20 rounded-full border border-neutral-300 flex items-center justify-center cursor-pointer hover:bg-red-700 bg-red-500 duration-300 group-hover:flex hidden">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6 text-white"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
-
-      <main className="flex-1 container mx-auto p-4">
+      <main className="w-full mx-auto py-4 px-12">
         <Outlet />
       </main>
-
-      <footer className="border-t border-neutral-800 p-4 text-center text-neutral-500 text-sm">
-        &copy; {new Date().getFullYear()} HumanOps Live
-      </footer>
     </div>
   );
 };
